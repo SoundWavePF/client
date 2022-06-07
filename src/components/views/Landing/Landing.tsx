ab9a8b82d5d6553e56e11a57624b286ebc2fb06e
nanodebortoli <nano.debortoli@hotmail.com>
2022-06-07 16:23:56 -0300

    feat(landing): created landing

--- src/components/views/Landing/Landing.tsx
+++ src/components/views/Landing/Landing.tsx
@@ -1,7 +1,14 @@
+import { Link } from 'react-router-dom';
+import styles from './Landing.module.css';
 
-const Landing = () => {
-  return (
-    <h1>La landing</h1>
+export default function(){
+  return(
+    <div className={styles.containerBig}>
+      <div className={styles.container}>
+        <h1>SoundWave</h1>
+        <p>Register now on the best platform to play your favorite music</p>
+        <div className={styles.button}><Link to='/player' className={styles.button}>Open Player</Link></div>
+      </div>
+    </div>
   )
 }
-export default Landing
