e6c8a9b56048f527cfe816f25ebd2616305e5df7
nanodebortoli <nano.debortoli@hotmail.com>
2022-06-07 17:04:50 -0300

    refactor(landing): changed welcome text

--- src/components/views/Landing/Landing.tsx
+++ src/components/views/Landing/Landing.tsx
@@ -5,8 +5,8 @@ export default function(){
   return(
     <div className={styles.containerBig}>
       <div className={styles.container}>
-        <h1>SoundWave</h1>
-        <p>Register now on the best platform to play your favorite music</p>
+        <h1 className={styles.title}>SoundWave</h1>
+        <p className={styles.p}>Sign up now on the best platform to play and share your favorite music</p>
         <div className={styles.button}><Link to='/player' className={styles.button}>Open Player</Link></div>
       </div>
     </div>
