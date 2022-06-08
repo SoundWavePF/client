fec985096547573fa2ad90246314b1014a6d71b0
Fran Mansilla <75451439+FranciscoMansilla@users.noreply.github.com>
2022-06-07 23:05:27 -0300

    feat(F:\HENRY\PF\soundWave\client\src\components\commons\SignUpForm\SignUpForm.tsx)
    formulario

--- src/components/views/SignUp/SignUp.tsx
+++ src/components/views/SignUp/SignUp.tsx
@@ -1,6 +1,10 @@
 import { GoogleLogin } from 'react-google-login';
+import { Link } from 'react-router-dom';
+import SignUpForm from '../../commons/SignUpForm/SignUpForm';
+
 
 const SignUp = ()=>{
+  
   const responseGoogle = (response:any)=>{
     console.log(response)
   }
@@ -8,8 +12,8 @@ const SignUp = ()=>{
     <div>
       <div><img src="https://i.pinimg.com/originals/39/ba/5f/39ba5f08327af406e200f3c6d1dff922.jpg" alt="img_signUp" /></div>
       <div>
+        <Link to='/'>back to landing</Link>
         <h2>Sign up for free to listen.</h2>
-        <button>Sign Up with Facebook?</button>
         <GoogleLogin
           clientId="436078684823-tdtk9tntpl5cmpqj7an47rtoq6h0q652.apps.googleusercontent.com"
           buttonText="Login"
@@ -21,6 +25,7 @@ const SignUp = ()=>{
           cookiePolicy={'single_host_origin'}
         />
         <hr />
+        <SignUpForm />
       </div>
     </div>
   )
