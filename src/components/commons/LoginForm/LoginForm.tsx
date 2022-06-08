b72f8112db515186d1d25d0f93d11df0c9ad6afa
Fran Mansilla <75451439+FranciscoMansilla@users.noreply.github.com>
2022-06-08 13:50:52 -0300

    feat(src/LoginForm) add files
    add file LoginForm.tsx
    add file LoginForm.module.css

---
+++ src/components/commons/LoginForm/LoginForm.tsx
@@ -0,0 +1,66 @@
+import { useState } from "react"
+import m from'./LoginForm.module.css'
+
+const LoginForm = ()=>{
+  const [form, setForm] = useState({
+    email:'',
+    password:''
+  })
+  const [error,setError] = useState({
+    email:'',
+    password:''
+  })
+  const validateEmail = (value:string):void =>{
+    if(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(value)){
+      setError({...error,email:''})
+    }
+    else{
+      setError({...error,email:'You have entered an invalid email address!'})
+    }
+    setForm({...form, email:value})
+  }  
+  const validatePassword = (value:string):void =>{
+    if(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]{8,}$/.test(value)){
+      setError({...error,password:''})
+    }
+    else{
+      setError({...error,password:'You have entered an invalid password'})
+    }
+    setForm({...form, password:value})
+  }
+  const disabled = ():boolean =>{
+    if(!form.email)return true
+    if(!form.password)return true
+    if(error.email)return true
+    if(error.password)return true
+    return false
+  }
+  const onSubmitSignUp = (e:React.FormEvent)=>{
+    e.preventDefault()
+    console.log(e)
+    console.log(form)
+  }
+  return(
+    <div >
+      <form className={m.container_w4s5} onSubmit={(e)=>{onSubmitSignUp(e)}}>
+        <input className={m.inputForm_7fv8} 
+          type="email"
+          placeholder="Put your email." 
+          name='email' 
+          value={form.email}
+          onChange={(e)=>validateEmail(e.target.value)}   
+      /> <label>{error.email}</label>
+      <input className={m.inputForm_7fv8}
+          type="password"
+          placeholder="Create a password." 
+          name='password' 
+          value={form.password}
+          onChange={(e)=>validatePassword(e.target.value)}   
+      /> <label>{error.password}</label>
+      <button className={m.inputSubmit_yu79}
+        disabled={disabled()} type="submit">Register</button>
+      </form>
+    </div>
+  )
+}
+export default LoginForm
