4e2623275eb826f072a192fae66d72ae17d56778
riosgas <78388221+riosgas@users.noreply.github.com>
2022-06-08 11:50:10 -0300

    feat(/home): add component
    pendiente de importar los componentes correspondientes

---
+++ src/components/views/Home/Home.tsx
@@ -0,0 +1,18 @@
+import styles from "./Home.module.css";
+
+// import SearchBar from "./components/";
+// import SideBar from "./components/";
+// import Welcome? from "./components/";
+// import Player from "./components/";
+
+const Home = () => {
+  return (
+    <div className={styles.container}>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
+  );
+};
+export default Home;
