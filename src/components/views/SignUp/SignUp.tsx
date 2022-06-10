edf22c755fcc0475323f0d7611b116d71162496b
Fran Mansilla <75451439+FranciscoMansilla@users.noreply.github.com>
2022-06-10 17:28:22 -0300

    ya no hay mas libreria de google

--- src/components/views/SignUp/SignUp.tsx
+++ src/components/views/SignUp/SignUp.tsx
@@ -1,7 +1,5 @@
-import { GoogleLogin } from 'react-google-login';
 import { Link } from 'react-router-dom';
 import SignUpForm from '../../commons/SignUpForm/SignUpForm';
-import * as b from "react-bootstrap";
 import m from './SignUp.module.css'
 import NavBar from '../../commons/NavBar/NavBar';
 
@@ -18,16 +16,7 @@ const SignUp = ()=>{
         <div className={m.containerform_98ajz}>
           <Link className={m.linkTolanding_h8z6} to='/'>back to landing</Link>
           <h2 className={m.h2SignUp_f8h3}>Sign up for free to listen.</h2>
-          <GoogleLogin
-            clientId="436078684823-tdtk9tntpl5cmpqj7an47rtoq6h0q652.apps.googleusercontent.com"
-            buttonText="Login"
-            render={renderProps => (
-              <button className={m.inputGoogle_7fv8 } onClick={renderProps.onClick} disabled={renderProps.disabled}>Sign Up with Google</button>
-              )}
-              onSuccess={responseGoogle}
-              onFailure={responseGoogle}
-              cookiePolicy={'single_host_origin'}
-              />
+          <button className={m.inputGoogle_7fv8 }>Sign Up with Google</button>
           <SignUpForm />
         </div>
       </div>
