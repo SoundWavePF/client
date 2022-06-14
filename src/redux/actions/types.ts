8b12a6d2e4840770f5ae0104ff6e8831f38d27e4
Fran Mansilla <75451439+FranciscoMansilla@users.noreply.github.com>
2022-06-14 14:24:54 -0300

    conexion con reducer del los componentes de Genre

--- src/redux/actions/types.ts
+++ src/redux/actions/types.ts
@@ -1,8 +1,10 @@
 import { LibraryArtist } from "../reducer/types";
 
-export type Actions = GetGenres | GetAlbum | ChangeAdminOption | PostSignUp | PostLogin | SearchAll | GetChart | GetLastSongs | SendPrevPlay | PlaySong | AddToQueue | getLibrary;
+export type Actions = GetGenres | GetAlbum | ChangeAdminOption | PostSignUp | PostLogin | SearchAll | GetChart | GetLastSongs | SendPrevPlay | PlaySong | AddToQueue | getLibrary | GetGenre | CleanGenre;
 
 export enum ActionType{
+  CLEAN_GENRE = 'CLEAN_GENRE',
+  GET_GENRE = 'GET_GENRE',
   GET_GENRES = 'GET_GENRES',
   GET_CHART = 'GET_CHART',
   GET_LAST_SONGS = 'GET_LAST_SONGS',
@@ -22,7 +24,13 @@ export interface userOption{ //opciones del componente AdminPanel
   home:boolean,
   user:boolean
 }
-
+interface CleanGenre{
+  type: ActionType.CLEAN_GENRE;
+}
+interface GetGenre{
+  type: ActionType.GET_GENRE;
+  payload?: any
+}
 interface GetGenres{
   type: ActionType.GET_GENRES;
   payload?: swGenre[]
