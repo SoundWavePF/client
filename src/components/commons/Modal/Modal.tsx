23d76d5676aaf0f7e19c3264766b643749a7320a
Jonathan Jesus Hortman <99038380+JonathanHortman@users.noreply.github.com>
2022-06-15 01:37:23 -0300

    feat/userSettings & artist form

--- src/components/commons/Modal/Modal.tsx
+++ src/components/commons/Modal/Modal.tsx
@@ -5,42 +5,75 @@ interface Modal {
     handleModal: any
     header: string
     oldData?: string
+    artistName?: string
     description: string
     label: string
     contentButton: string
     type: string
+    setpass?: string
+    inputArtistType?:string
 }
 
 const Modal = (props: Modal) => {
-    const modalx = { target: { name: "all" } }
-    console.log(modalx)
-    console.log(props.handleModal)
     return (
         <div className={style.modalContainer}>
             <div className={style.modal}>
-                
 
-                    <div className={style.parent}>
-                        <div className={style.div1}><h1>{props.header}</h1></div>
-                        <div className={style.div2}> <button onClick={() => props.handleModal(modalx)}>X</button></div>
-                        <div className={style.div3}><h3>{props.description}</h3></div>
-                        <div className={style.div4}>{
+
+                <div className={style.parent}>
+                    <div className={style.div1}><h1>{props.header}</h1></div>
+                    <div className={style.div2}><button onClick={(e) => props.handleModal(e)} name="all">X</button></div>
+                    <div className={style.div3}><h3>{props.description}</h3></div>
+                    <div className={style.div4}>
+                        {
                             props.oldData ?
                                 <div><label>{props.oldData}</label></div>
                                 : null
-                        }</div>
-                        <div className={style.div5}>{
+                                
+                        }
+                        {
+                            props.artistName ?
+                                <div><label>{props.setpass}</label></div>
+                                : null
+                        }
+                    </div>
+                    <div className={style.div5}>
+                        {
                             props.oldData ?
                                 <input type={props.type} />
                                 : null
-                        }</div>
-                        <div className={style.div6}><label>{props.label}</label></div>
-                        <div className={style.div7}><input type={props.type} /></div>
-                        <div className={style.div8}><button>{props.contentButton}</button></div>
+                        }
+                        {
+                            props.artistName ?
+                                <input type={props.inputArtistType} />
+                                : null
+                        }
                     </div>
+                    <div className={style.div6}>
+                        {
+                            props.artistName ?
+                                <div><label>{props.artistName}</label></div>
+                                : null
+                        }
+                    </div>
+                    <div className={style.div7}>
+                        {
+                            props.artistName ?
+                                <div><input type={props.type} /></div>
+                                : null
+
+                        }
+                    </div>
+                    <div className={style.div8}><label>{props.label}</label></div>
+                    <div className={style.div9}><input type={props.type} /></div>
+                    <div className={style.div10}><button>{props.contentButton}</button></div>
+                </div>
+
 
             </div>
+
         </div>
+
     )
 }
 
