dd4d25c3717cb7c6c51e615e3cbc5d5c39d3461a
Fran Mansilla <75451439+FranciscoMansilla@users.noreply.github.com>
2022-06-15 11:52:09 -0300

    cambios varios

--- src/components/commons/CardContainer/CardItem.tsx
+++ src/components/commons/CardContainer/CardItem.tsx
@@ -50,12 +50,12 @@ const CardItem: React.FC<myProps> = (props: myProps) => {
     case 'track':
         return(
           <div className={styles.song}>
-            <figure><img src={props.item.album.image_medium ?props.item.album.image_medium :props.item.image_medium}
+            <figure><img src={props.item.album?.image_medium ?props.item.album.image_medium :props.item.image_medium}
               alt={props.item.title}
               onClick={() => playSong(props.item)}
             /></figure>
-            <p>{props.item.title}</p>
-            <a href={`/artist/${props.item.id}`}>{props.item.artist}</a>
+            <p>{props.item.name}</p>
+            <a href={`/artist/${props.item.id}`}>{props.item.artists[0].name}</a>
           </div>
         )
     default:
--- src/components/commons/NavBar/NavBar.tsx
+++ src/components/commons/NavBar/NavBar.tsx
@@ -4,20 +4,22 @@ import logo from '../../../assets/logo.png';
 import { Link } from "react-router-dom";
 import { useEffect, useState } from "react";
 import jwt_decode from "jwt-decode";
+import { useAuth0 } from "@auth0/auth0-react";
 
 const NavBar = () => {
   const [username, setUsername] = useState('')
+  const {user, isAuthenticated, loginWithRedirect} = useAuth0();
 
-  useEffect(() => {
-    const token = localStorage.getItem('sw-token') || '{}';
-    if (token !== '{}') {
-      const decoded:any = jwt_decode(token);
-      setUsername(decoded.username);
-      console.log(decoded);
-    } else{
-      setUsername('Unregistered')
-    }
-  }, []);
+  // useEffect(() => {
+  //   const token = localStorage.getItem('sw-token') || '{}';
+  //   if (token !== '{}') {
+  //     const decoded:any = jwt_decode(token);
+  //     setUsername(decoded.username);
+  //     console.log(decoded);
+  //   } else{
+  //     setUsername('Unregistered')
+  //   }
+  // }, []);
 
   return (
     <div className={styles.container}>
@@ -29,12 +31,13 @@ const NavBar = () => {
       <Link to='/'>
         <img src={logo} alt="logo" />
       </Link>
-      {username === 'Unregistered' && 
-            <div>
-            <Link to='/signup' className="btn btn-warning">Sign Up</Link>
-            <Link to='/login' className="btn btn-outline-warning">Log In</Link>
-          </div>
-      }
+      {/* {username === 'Unregistered' &&  */}
+        
+            {/* <Link to='/signup' className="btn btn-warning">Sign Up</Link>
+            <Link to='/login' className="btn btn-outline-warning">Log In</Link> */}
+            <button onClick={()=>loginWithRedirect()} className="btn btn-outline-warning" >Log In</button>
+        
+    
 
     </div>
   );
