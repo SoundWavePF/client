b984912f5549a0b96e80123d895bbd5911861e61
Larryc8 <yeisonlarry1954@gmail.com>
2022-06-17 18:54:03 -0500

    drag and drop

--- src/components/commons/Player/QueuePanel.jsx
+++ src/components/commons/Player/QueuePanel.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useRef, useEffect } from 'react';
 import {useSelector} from 'react-redux';
+import {ReactSortable} from "react-sortablejs"
 
 import style from './QueuePanel.module.css';
 
@@ -8,25 +9,30 @@ image_small
 name
 */
 const Song = (props)=>{
-    
+
     function handleClick(){
         alert('play')
     }
 
     return (
-        <tr className={style.song } >
-            <td className={style.name}  onClick={handleClick}>
+        <div className={style.song } >
+            <div className={style.name}  onClick={handleClick}>
                 <img className={style.image} src={props.item.image_small} alt="" /> 
-                <div>{props.index}. {props.item.name}</div> 
-            </td>
-            <td className={style.info}><img width={'20px'} src={'https://cdn-icons.flaticon.com/png/512/3405/premium/3405244.png?token=exp=1655499956~hmac=b249cc39a084dc454c2b40090aea3340'}/> </td>
-        </tr>
+                {props.index} {props.item.name}
+            </div>
+            <div className={style.info}><img width={'20px'} src={'https://cdn-icons.flaticon.com/png/512/3405/premium/3405244.png?token=exp=1655499956~hmac=b249cc39a084dc454c2b40090aea3340'}/> </div>
+        </div>
     )
 }
 
 const QueuePanel = ()=>{
     const [open, setOpen] = useState(false);
     const queue = useSelector(state=>state.queue)
+    const [state, setState] = useState(queue);
+    
+    console.log('state', state)
+    console.log('queue', queue)
+
     const container = useRef(null); 
 
     console.log(queue)
@@ -61,9 +67,14 @@ const QueuePanel = ()=>{
         {
             !open ? null :
                 <div className={style.box}>
-                        <table className={style.songTable}>
-                            {queue.map( song => <Song item={song} index={1} />)}
-                        </table>
+                    <h4>Queue</h4>
+                    <hr/>
+                    <ReactSortable list={state} setList={setState}>
+                        {state.map((song) => {
+                            console.log('state', state)
+                            return <Song item={song} index={1} />
+                        } )}
+                    </ReactSortable>
                 </div>
         }
     </div>
