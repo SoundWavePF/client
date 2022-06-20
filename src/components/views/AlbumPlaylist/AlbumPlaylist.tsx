58d473e4ef0d90371d2d2c6d4cf86af19f9ca30b
Fran Mansilla <75451439+FranciscoMansilla@users.noreply.github.com>
2022-06-20 12:22:39 -0300

    se muestran busquedas en la vista de albums

--- src/components/views/AlbumPlaylist/AlbumPlaylist.tsx
+++ src/components/views/AlbumPlaylist/AlbumPlaylist.tsx
@@ -2,13 +2,22 @@ import styles from "../Home/Home.module.css";
 import SearchBar from "../../commons/SearchBar/SearchBar";
 import SideBar from "../../commons/SideBar/SideBar";
 import ContentAlbumPlaylist from "../../commons/ContentAlbumPlaylist/ContentAlbumPlaylist";
+import { useSelector } from "react-redux";
+import SearchResults from "../SearchResults/SearchResults";
 
 const AlbumPlaylist = () => {
+  const searchString = useSelector((state: any) => state.query)
+  
   return (
     <div className={styles.container}>
       <SearchBar/>
       <SideBar/>
-      <ContentAlbumPlaylist/>
+      {
+        !searchString ?
+          <ContentAlbumPlaylist/>
+          :
+          <SearchResults />
+      }
     </div>
   );
 };
