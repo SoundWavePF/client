b0334348f6efad4809dae8f4930fd91f1d9b57d1
cristian18u <98076070+cristian18u@users.noreply.github.com>
2022-06-24 00:17:35 -0500

    playlist funcionalidad mejoras

--- src/components/commons/CardContainer/CardItem.tsx
+++ src/components/commons/CardContainer/CardItem.tsx
@@ -1,25 +1,27 @@
 import styles from "./CardItem.module.css";
 import { Link } from "react-router-dom";
 import * as actionCreator from "../../../redux/actions/action_player";
+import * as actionCreator2 from "../../../redux/actions/action_user";
 import { useDispatch } from "react-redux";
 import { bindActionCreators } from "redux";
-import playlistFran from '../../../assets/playlistFran2.png';
-import DropDownButton from '../DropDownButton/DropDownButton';
+import playlistFran from "../../../assets/playlistFran2.png";
+import DropDownButton from "../DropDownButton/DropDownButton";
+import { useAuth0 } from "@auth0/auth0-react";
 
 interface myProps {
-  item: any
+  item: any;
 }
 
 const CardItem: React.FC<myProps> = (props: myProps) => {
   let IType = props.item.type;
+  const { user } = useAuth0();
+  const email = user?.email;
   const dispatch = useDispatch();
-  const { playSong } = bindActionCreators(
-    actionCreator,
-    dispatch
-  );
+  const { playSong } = bindActionCreators(actionCreator, dispatch);
+  const { deletePlaylist } = bindActionCreators(actionCreator2, dispatch);
   switch (IType) {
-    case 'genre':
-      return(
+    case "genre":
+      return (
         <div>
           <Link to={`/genre/${props.item.id}`} className={styles.alt}>
             <p>{props.item.name}</p>
@@ -28,43 +30,62 @@ const CardItem: React.FC<myProps> = (props: myProps) => {
             {props.item.name==='Rock' && <img src='https://e-cdns-images.dzcdn.net/images/misc/8ad3050b34360d0573d9d7b8bf38997d/134x264-000000-80-0-0.jpg' alt={props.item.name}/>}
             {props.item.name==='Reggaeton' && <img src='https://e-cdns-images.dzcdn.net/images/misc/dfd2c5f4b1ea497efb2f82da98b73572/134x264-000000-80-0-0.jpg' alt={props.item.name}/>}
             {props.item.name==='Rap/Hip Hop' && <img src='https://e-cdns-images.dzcdn.net/images/misc/b8ae8f0791f5e9e6c7a11bab94b0cbad/134x264-000000-80-0-0.jpg' alt={props.item.name}/>} */}
-            <img src='https://e-cdns-images.dzcdn.net/images/misc/8ad3050b34360d0573d9d7b8bf38997d/134x264-000000-80-0-0.jpg' alt={props.item.name}/>
+            <img
+              src="https://e-cdns-images.dzcdn.net/images/misc/8ad3050b34360d0573d9d7b8bf38997d/134x264-000000-80-0-0.jpg"
+              alt={props.item.name}
+            />
           </Link>
         </div>
-      )
-    case 'album':
-      return(
+      );
+    case "album":
+      return (
         <div className={styles.default}>
           <Link to={`/album/${props.item.id}`}>
-            <img src={props.item.image_medium} alt={props.item.title}/>
+            <img src={props.item.image_medium} alt={props.item.title} />
           </Link>
           <p>{props.item.name}</p>
         </div>
-      )
-    case 'playlist':
-      return(
+      );
+    case "playlist":
+      return (
         <div className={styles.default}>
           <Link to={`/playlist/${props.item.id}`}>
-            <img src={playlistFran} alt={props.item.name}/>
+            <img src={playlistFran} alt={props.item.name} />
           </Link>
           <p>{props.item.name}</p>
+          <button onClick={() => email && deletePlaylist(props.item.id)}>
+            x
+          </button>
         </div>
-      )
-    case 'track':
-        return(
-          <div className={styles.song}>
-            <figure className={styles.figure}><img src={props.item.album?.image_medium ? props.item.album.image_medium : props.item.image_medium}
+      );
+    case "track":
+      return (
+        <div className={styles.song}>
+          <figure className={styles.figure}>
+            <img
+              src={
+                props.item.album?.image_medium
+                  ? props.item.album.image_medium
+                  : props.item.image_medium
+              }
               alt={props.item.title}
               onClick={() => playSong(props.item)}
             />
-            <ul className={styles.actions}><li className={styles.action}><DropDownButton item={props.item}/></li><li className={styles.action}>like</li></ul>
-            </figure>
-            <p>{props.item.name}</p>
-            <a href={`/artist/${props.item.artists[0].id}`}>{props.item.artists[0].name}</a>
-          </div>
-        )
+            <ul className={styles.actions}>
+              <li className={styles.action}>
+                <DropDownButton item={props.item} />
+              </li>
+              <li className={styles.action}>like</li>
+            </ul>
+          </figure>
+          <p>{props.item.name}</p>
+          <a href={`/artist/${props.item.artists[0].id}`}>
+            {props.item.artists[0].name}
+          </a>
+        </div>
+      );
     default:
-      return <></>
+      return <></>;
   }
 };
 export default CardItem;
