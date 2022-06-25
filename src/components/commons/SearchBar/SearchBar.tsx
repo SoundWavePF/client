d02abe8da7ce6cb4168d6ca785828cf13dd08c04
Larryc8 <yeisonlarry1954@gmail.com>
2022-06-25 11:12:29 -0500

    better login button

--- src/components/commons/SearchBar/SearchBar.tsx
+++ src/components/commons/SearchBar/SearchBar.tsx
@@ -11,17 +11,18 @@ import MenuUser from "./MenuUser";
 import { useAuth0 } from "@auth0/auth0-react";
 
 const LoginButton: React.FunctionComponent = ()=>{
+  const { loginWithRedirect } = useAuth0()
   return (
-    <button className={style.buttonLogin}> Login
-  <div className={style.icon}>
-    <svg height="24" width="24" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-      <path d="M0 0h24v24H0z" fill="none">
-      </path>
-      <path d="M16.172 11l-5.364-5.364 1.414-1.414L20 12l-7.778 7.778-1.414-1.414L16.172 13H4v-2z" fill="currentColor">
-      </path>
-      </svg>
-  </div>
-</button>
+  <button className={style.buttonLogin} onClick={() => loginWithRedirect()}> Login
+    <div className={style.icon}>
+      <svg height="24" width="24" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+        <path d="M0 0h24v24H0z" fill="none">
+        </path>
+        <path d="M16.172 11l-5.364-5.364 1.414-1.414L20 12l-7.778 7.778-1.414-1.414L16.172 13H4v-2z" fill="currentColor">
+        </path>
+        </svg>
+    </div>
+  </button>
   )
 }
 
