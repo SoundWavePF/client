2dc8bf705810db3e6454e531eccdbd5b675ea1ff
Jonathan Jesus Hortman <99038380+JonathanHortman@users.noreply.github.com>
2022-06-24 22:36:15 -0300

    update/uploadMusic - responsive

--- src/components/commons/FloatButton/FloatButton.tsx
+++ src/components/commons/FloatButton/FloatButton.tsx
@@ -21,6 +21,7 @@ const FloatButton = () => {
                 })
 
             case "all":
+                
                 return setModal({
                     modalOptions: false,
                 })
@@ -35,14 +36,14 @@ const FloatButton = () => {
     return (
         <div>
             <button className={style.buttonOpenModal} onClick={(e) => handleModal(e)} name='modalOptions' />
-            <img className={style.firstBackground} src="https://images.unsplash.com/photo-1602250829481-12309c45eb63?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80" alt="" />
+            <img className={style.firstBackground} src="https://images.unsplash.com/photo-1602250829481-12309c45eb63?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80" alt="background" />
             <div className={style.secondBackground} />
             {
                 modal.modalOptions === true ?
                     <div className={style.containerCreates}>
                         <div className={style.modalExistingAlbum}>
                             <FileUpload />
-                            <button className={style.buttonClose} onClick={(e) => handleModal(e)} name='all'>x</button>
+                            <button className={style.buttonClose} onClick={(e) => handleModal(e)} name='all'>X</button>
                         </div>
                     </div>
                     : null
