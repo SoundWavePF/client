567301ba8085de9e0d950222e1bde9da69fee107
Fran Mansilla <75451439+FranciscoMansilla@users.noreply.github.com>
2022-06-24 22:14:17 -0300

    refactor sweetAlert in /DonationsBurtton

--- src/components/commons/ArtistPage/DonationsButton/DonationsButton.tsx
+++ src/components/commons/ArtistPage/DonationsButton/DonationsButton.tsx
@@ -5,33 +5,52 @@ import axios from "axios";
 
 const DonationsButton = (props:any)=>{
   const swal:any = Swal
-
+  
   const donate = (id:any, email:string, stripe:any)=>{
     swal.fire({
+      icon:'question',
       title: 'How much do you want to donate?',
-      icon: 'question',
-      input: 'range',
-      inputLabel: 'The amount',
-      inputAttributes: {
-        min: 1,
-        max: 200,
-        step: 1
-      },
-      inputValue: 15
-    })
-    .then((r:any)=>{
-      if(r.value){
-        let donation = { 
-          artistId:id,
-          userEmail:email, 
-          amount: Number(r.value), 
-          stripeId: stripe 
+      input: 'text',
+      inputLabel: 'The amount is in USD',
+      confirmButtonColor: '#ffee32',
+      cancelButtonColor: '#dd6b55',
+      showCancelButton: true,
+      inputValidator: (value:any) => {
+        console.log('ACA value',value)
+        if (!/^\s*-?[1-9]\d*(\.\d{1,2})?\s*$/.test(value)) {
+          swal.fire({
+            icon: 'error',
+            title: 'Error...',
+            text: 'Enter a number amount!',
+            confirmButtonColor: '#ffee32',
+          })
+        }else{
+          if(value<=0){
+            swal.fire({
+              icon: 'error',
+              title: 'Error...',
+              text: 'Enter an amount greater than zero!',
+              confirmButtonColor: '#ffee32',
+            })
+          }else{
+            let donation = { 
+              artistId:id,
+              userEmail:email, 
+              amount: Number(value), 
+              stripeId: stripe 
+            }
+            swal.fire({
+              title:'Success!', 
+              text:'Redirecting', 
+              icon:'success',
+              confirmButtonColor: '#ffee32',
+            })
+            axios.post('https://www.javierochoa.me/order',donation)
+            .then(({data}:any)=>{
+              window.location.href = data.url
+            })
+          }
         }
-      axios.post('https://www.javierochoa.me/order',donation)
-        .then(({data}:any)=>{
-          window.location.href = data.url
-        })
-        console.log(donation)
       }
     })
   } 
