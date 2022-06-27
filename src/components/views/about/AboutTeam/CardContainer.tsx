664408c21c3325643712ab40043d22b07188b411
Fran Mansilla <75451439+FranciscoMansilla@users.noreply.github.com>
2022-06-26 21:14:58 -0300

    refactor /About2

--- src/components/views/about/AboutTeam/CardContainer.tsx
+++ src/components/views/about/AboutTeam/CardContainer.tsx
@@ -15,57 +15,54 @@ import gaston from '../../../../assets/about/gaston.png'
 const  AboutCardContainer=( )=> {
   return (
         <div className={Style.cardContainer}>
-            <div>
-              <AboutCard 
-              name={'Francisco Mansilla'}        
-                group={'FrontEnd Developer'} 
-                img={fran}      
-                git={'https://github.com/FranciscoMansilla'} 
-                lin={'https://www.linkedin.com/in/francisco-mansilla-5a6667237/'}></AboutCard>
-                <AboutCard 
-                name={'Cristian Vanegas'}           
-                group={'BackEnd Developer'}  
-                img={cris}                                                                                                                      
-                git={'https://github.com/cristian18u'}       lin={''} ></AboutCard>
-              <AboutCard 
-                name={'Javier Ochoa'}              
-                group={'BackEnd Developer'}  
-                img={javier}      
-                git={'https://github.com/JavierOchoa'}       
-                lin={'https://www.linkedin.com/in/javierochoaalv/'}></AboutCard>
-              <AboutCard 
-                name={'Jonathan Jesus Hortman '}   
-                group={'FrontEnd Developer'} img={joni}                                                                                                                                  
-                git={'https://github.com/JonathanHortman'}   
-                lin={'https://www.linkedin.com/in/jonathan-jesus-hortman-5ab210236/'}></AboutCard>
-            </div>
-            <div>
-              <AboutCard 
-                name={'Gastón Ríos'}               
-                group={'FrontEnd Developer'} 
-                img={gaston}      
-                git={'https://github.com/riosgas'}           
-                lin={'https://www.linkedin.com/in/gaston-rios/'} ></AboutCard>
-              <AboutCard 
-                name={'Ignacio Debórtoli'}         
-                group={'FrontEnd Developer'} 
-                img={nano}                                                                                                                      
-                git={'https://github.com/nanodebortoli'}     
-                lin={'https://www.linkedin.com/in/ignaciodebortoli'} ></AboutCard>
-              <AboutCard 
-                name={'Harold Riascos '}           
-                group={'FrontEnd Developer'} 
-                img={harold}      
-                git={'https://github.com/Larryc8'}           
-                lin={'https://www.linkedin.com/in/harold-andres-riascos-manyoma-980748238/'} ></AboutCard>
-              <AboutCard 
-                name={'David Natanael Gomez'}      
-                group={'FrontEnd Developer'} 
-                img={david}                                                                                 
-                git={'https://github.com/davidnatanel'}      
-                lin={'https://www.linkedin.com/in/david-natanael-gomez-348478216/'} ></AboutCard>
-            </div>
-          </div>
+          <AboutCard 
+            name={'Francisco Mansilla'}        
+            group={'FrontEnd Developer'} 
+            img={fran}      
+            git={'https://github.com/FranciscoMansilla'} 
+            lin={'https://www.linkedin.com/in/francisco-mansilla-5a6667237/'}></AboutCard>
+          <AboutCard 
+            name={'Cristian Vanegas'}           
+            group={'BackEnd Developer'}  
+            img={cris}                                                                                                                      
+            git={'https://github.com/cristian18u'}       lin={''} ></AboutCard>
+          <AboutCard 
+            name={'Javier Ochoa'}              
+            group={'BackEnd Developer'}  
+            img={javier}      
+            git={'https://github.com/JavierOchoa'}       
+            lin={'https://www.linkedin.com/in/javierochoaalv/'}></AboutCard>
+          <AboutCard 
+            name={'Jonathan Jesus Hortman '}   
+            group={'FrontEnd Developer'} img={joni}                                                                                                                                  
+            git={'https://github.com/JonathanHortman'}   
+            lin={'https://www.linkedin.com/in/jonathan-jesus-hortman-5ab210236/'}></AboutCard>
+          <AboutCard 
+            name={'Gastón Ríos'}               
+            group={'FrontEnd Developer'} 
+            img={gaston}      
+            git={'https://github.com/riosgas'}           
+            lin={'https://www.linkedin.com/in/gaston-rios/'} ></AboutCard>
+          <AboutCard 
+            name={'Ignacio Debórtoli'}         
+            group={'FrontEnd Developer'} 
+            img={nano}                                                                                                                      
+            git={'https://github.com/nanodebortoli'}     
+            lin={'https://www.linkedin.com/in/ignaciodebortoli'} ></AboutCard>
+          <AboutCard 
+            name={'Harold Riascos '}           
+            group={'FrontEnd Developer'} 
+            img={harold}      
+            git={'https://github.com/Larryc8'}           
+            lin={'https://www.linkedin.com/in/harold-andres-riascos-manyoma-980748238/'} ></AboutCard>
+          <AboutCard 
+            name={'David Natanael Gomez'}      
+            group={'FrontEnd Developer'} 
+            img={david}                                                                                 
+            git={'https://github.com/davidnatanel'}      
+            lin={'https://www.linkedin.com/in/david-natanael-gomez-348478216/'} ></AboutCard>
+          
+        </div>
     );
 }
 
