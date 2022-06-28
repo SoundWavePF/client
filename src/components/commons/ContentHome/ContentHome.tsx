b33ca9b92991f2f2f2f8645080c592626da6017d
Larryc8 <yeisonlarry1954@gmail.com>
2022-06-28 15:08:48 -0500

    responsive carrusel

--- src/components/commons/ContentHome/ContentHome.tsx
+++ src/components/commons/ContentHome/ContentHome.tsx
@@ -13,12 +13,17 @@ const ContentHome = () => {
   const { getGenres, getLastSongs, getChart, getTop, getDiscoverSongs } = bindActionCreators(actionCreator,dispatch);
   const { last, genres, chart, discover, queue } = useSelector((state: any) => state.home);
   const top = useSelector((state: any) => state.top);
+  const [windowSize, setWindowSize] = useState<number>(window.innerWidth)
   
   useEffect(() => {
     if(discover.length<1) getDiscoverSongs()
     if(genres.length<1) getGenres();
     if(chart.length<1) getChart();
     if(top.length<1) getTop();
+
+    window.addEventListener('resize', ()=>{
+      setWindowSize(window.innerWidth);
+    })
   }, []);
 
   useEffect(() => {
@@ -46,13 +51,13 @@ const ContentHome = () => {
       { genres.length > 0 &&
         <div className={styles.section}>
           <h1>Genres</h1>
-          <CardContainer content={genres.slice(0,11).filter((genre:any)=>genre.name!=='Bolero')} />
+          <CardContainer content={genres.slice(0,11).filter((genre:any)=>genre.name!=='Bolero')} slides={windowSize <= 390}/>
         </div>
       }
       { chart.length > 0 &&
         <div className={styles.section}>
           <h1>Most popular</h1>
-          <CardContainer content={chart.slice(10)} slides={true}/>
+          <CardContainer content={chart.slice(11)} slides={true}/>
         </div>
       }
       { last?.length > 0 &&
