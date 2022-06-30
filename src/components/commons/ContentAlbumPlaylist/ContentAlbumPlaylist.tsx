667524361e0a4c7ed01fb1094abfe5cb154df8a4
Javier Ochoa <jochoaalv@gmail.com>
2022-06-29 20:50:35 -0500

    Update ContentAlbumPlaylist.tsx

--- src/components/commons/ContentAlbumPlaylist/ContentAlbumPlaylist.tsx
+++ src/components/commons/ContentAlbumPlaylist/ContentAlbumPlaylist.tsx
@@ -111,7 +111,7 @@ const ContentAlbumPlaylist = () => {
             Play all
           </button>
           {
-            isPlaylist && 
+            isPlaylist && showEdit && 
               <button
                 onClick={() => email && id && borrar(id)}
                 className={styles.btn}
